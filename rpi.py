9741c046805a0d59890074dcca9bdef33e4ca8f8
Gare <1073177998@qq.com>
2020-12-22 18:55:22 +0800

    Update rpi.py

--- rpi.py
+++ rpi.py
@@ -3,41 +3,7 @@
 import socket
 import time
 import sys
-HOST_IP = "192.168.137.67" #import socket
-import time
-import sys
-SERVER_IP = "192.168.137.67" #IP address of raspberry
-SERVER_PORT = 8888
-print("Starting socket: TCP...")
-server_addr = (SERVER_IP, SERVER_PORT)
-socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-while True:
-    try:
-        print("Connecting to server @ %s:%d..." %(SERVER_IP, SERVER_PORT))
-        socket_tcp.connect(server_addr)
-        break
-    except Exception:
-        print("Can't connect to server,try it latter!")
-        time.sleep(1)
-        continue
-print("Please input:")
-while True:
-    try:
-        data = socket_tcp.recv(512)
-        if len(data)>0:
-            print("Received: %s" % data)
-            command=input()
-            socket_tcp.send(bytes(command.encode('utf-8')))
-            time.sleep(1)
-            continue
-    except Exception as e:
-        exc_type, exc_value, exc_traceback_obj = sys.exc_info()
-        print("exc_type: %s" % exc_type)
-        print("exc_value: %s" % exc_value)
-        print("exc_traceback_obj: %s" % exc_traceback_obj)
-        socket_tcp.close()
-        socket_tcp=None
-        sys.exit(1)
+HOST_IP = "192.168.137.10" #IP address of raspberry
 HOST_PORT = 8888
 print("Starting socket: TCP...")
 #1.create socket object:socket=socket.socket(family,type)
@@ -51,9 +17,9 @@ socket_tcp.listen(1)
 #4.waite for client:connection,address=socket.accept()
 socket_con, (client_ip, client_port) = socket_tcp.accept()
 print("Connection accepted from %s." %client_ip)
-socket_con.send("Welcome to RPi TCP server!")
+socket_con.send(bytes("Welcome to RPi TCP server!".encode('utf-8')))
 print("Receiving package...")
-###主循环
+###main loop
 while True:
     try:
         data=socket_con.recv(512)
@@ -62,6 +28,10 @@ while True:
             socket_con.send(data)
             time.sleep(1)
             continue
-    except Exception:
+    except Exception as e:
+        exc_type, exc_value, exc_traceback_obj = sys.exc_info()
+        print("exc_type: %s" % exc_type)
+        print("exc_value: %s" % exc_value)
+        print("exc_traceback_obj: %s" % exc_traceback_obj)
         socket_tcp.close()
         sys.exit(1)
